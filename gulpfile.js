bd5c629b70ef651211bad4776a39b7d41b4d99a0
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-14 23:27:01 -0300

    Compile js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const babel = require('gulp-babel')
+const browserify = require('gulp-browserify')
 
 gulp.task('sass', () => {
   return gulp.src('styles/**/*.{scss,sass}')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('styles'))
+    .pipe(gulp.dest('css'))
+})
+
+gulp.task('js', () => {
+  return gulp.src('scripts/**/*.js')
+    .pipe(browserify({
+      insertGlobals : true
+    }))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('js'))
 })
 
 gulp.task('watch', () => {
   gulp.watch('styles/**/*.{scss,sass}', ['sass'])
+  gulp.watch('scripts/**/*.js', ['js'])
 })
 
-gulp.task('default', ['sass'])
+gulp.task('default', ['sass', 'js'])
