bcf1398b856064639ff84ed27f7becfd437cb16b
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-16 23:35:23 -0300

    Finish the index view with News section

--- src/components/Home/News.js
+++ src/components/Home/News.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 class News extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      news: [
+        { id: 1, title: 'some good title', description: 'Some nice description for this good movie' },
+        { id: 2, title: 'some good title', description: 'Some nice description for this good movie' },
+        { id: 3, title: 'some good title', description: 'Some nice description for this good movie' }
+      ],
+      launch: [
+        { id: 4, title: 'some good title', description: 'Some nice description for this good movie' },
+        { id: 5, title: 'some good title', description: 'Some nice description for this good movie' },
+        { id: 6, title: 'some good title', description: 'Some nice description for this good movie' }
+      ]
+    }
+  }
   render () {
     return (
       <section className="section info container">
@@ -8,89 +24,13 @@ class News extends Component {
           <div className="col-sm-12 col-md-6">
             <h2>News Week</h2>
             <ul className="list-group">
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
+              {this.state.news.map(newItem => <NewsItem item={newItem} key={newItem.id} />)}
             </ul>
           </div>
           <div className="col-sm-12 col-md-6">
             <h2>Next Launch</h2>
             <ul className="list-group">
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="list-group-item">
-                <a href="#">
-                  <div className="row">
-                    <div className="col-md-3">
-                      <img src="https://unsplash.it/130/130" alt="Some movie" className="img-rounded img-responsive" />
-                    </div>
-                    <div className="col-md-9">
-                      <h4 className="list-group-item-heading">List group item heading</h4>
-                      <p className="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ad quo omnis minus corrupti explicabo sed molestiae debitis, facere possimus, aliquid sint, fuga delectus excepturi?</p>
-                    </div>
-                  </div>
-                </a>
-              </li>
+              {this.state.launch.map(launchItem => <NewsItem item={launchItem} key={launchItem.id} />)}
             </ul>
           </div>
         </div>
