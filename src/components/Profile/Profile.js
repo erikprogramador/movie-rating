14899c8d12e5f456602cd178e7ded81620043cc1
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-18 16:29:18 -0300

    Update Profile Component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,14 +4,40 @@ class Profile extends Component {
   render () {
     return (
       <div className="container">
-        <div class="page-header">
+        <div className="page-header">
           <h1>Erik Vanderlei</h1>
         </div>
 
         <div className="row">
-          <div className="col-md-8 col-sm-12"></div>
           <div className="col-md-4 col-sm-12 text-center">
-            <img src="http://unsplash.it/300/300" alt="Erik Vanderlei" class="img-rounded" />
+            <img src="http://unsplash.it/400/300" alt="Erik Vanderlei" className="img-rounded img-responsive" />
+          </div>
+          <div className="col-md-8 col-sm-12">
+            <form>
+              <div className="form-group">
+                <label>Username:</label>
+                <input type="text" placeholder="Username" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label>Email:</label>
+                <input type="email" placeholder="Email" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label>Full Name:</label>
+                <input type="text" placeholder="Full Name" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label>Password:</label>
+                <input type="password" placeholder="Password" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label>Password Confirmation:</label>
+                <input type="password" placeholder="Password Confirmation" className="form-control" />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-warning">Update Profile</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
